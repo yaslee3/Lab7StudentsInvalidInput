dd8477a0b5555ed8e1a255eb67d59079a2a660da
Yasmine Lee <yaslee3@yahoo.com>
2016-09-15 15:54:12 -0400

    Initial commit

---
+++ src/LabSeven/StudentInvalid.java
@@ -0,0 +1,38 @@
+package LabSeven;
+
+public class StudentInvalid {
+	
+	public static void main(String []args){// add IllegalArgumentException
+		
+		//Create a Scanner
+		
+		
+		// multidimensional array with names, hometown, and favor food
+
+		
+		
+		//Prompt the use to ask about a particular student
+		
+		
+		
+		//create While loop to take in choice
+		
+		
+		//add for loop to take in name length
+		
+		
+		//add println to enter a student number 1-20
+		
+		
+		// try catch is in a loop to check and see if input is from 1-20
+		
+		
+		
+		// sets a variable named personSelected to the index that
+		// corresponds with the index #
+
+		
+		
+	}
+
+}
